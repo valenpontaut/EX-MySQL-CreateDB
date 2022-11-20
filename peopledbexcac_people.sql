d4b222293057cd4cd559df29546eeb567b9891f9
Valentina Llanos Pontaut <105258399+valenpontaut@users.noreply.github.com>
2022-11-20 04:47:24 -0300

    Create DB

--- peopledbexcac_people.sql
+++ peopledbexcac_people.sql
@@ -1,55 +1,23 @@
--- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
---
--- Host: localhost    Database: peopledbexcac
--- ------------------------------------------------------
--- Server version	8.0.31
+/*Creo la base de datos y la defino como default schema*/
+CREATE DATABASE PEOPLEDBEXCAC;
+USE PEOPLEDBEXCAC;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Creo la tabla de personas*/
+CREATE TABLE people
+(id INT(11) NOT NULL AUTO_INCREMENT,
+nombre VARCHAR(40) NOT NULL,
+apellido VARCHAR(40) NOT NULL,
+edad TINYINT(2) NOT NULL,
+fecha TIMESTAMP NOT NULL,
+provincia VARCHAR(30) NOT NULL,
+PRIMARY KEY (id));
 
---
--- Table structure for table `people`
---
-
-DROP TABLE IF EXISTS `people`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `people` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(40) NOT NULL,
-  `apellido` varchar(40) NOT NULL,
-  `edad` tinyint NOT NULL,
-  `fecha` timestamp NOT NULL,
-  `provincia` varchar(30) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `people`
---
-
-LOCK TABLES `people` WRITE;
-/*!40000 ALTER TABLE `people` DISABLE KEYS */;
-INSERT INTO `people` VALUES (1,'India','Rosales',73,'2022-11-20 07:05:22','La Pampa'),(2,'Tad','English',52,'2022-11-20 07:05:22','Buenos Aires'),(3,'Quamar','Francis',20,'2022-11-20 07:05:22','Tucumán'),(4,'Ronan','Holloway',46,'2022-11-20 07:05:22','Jujuy'),(5,'Kathleen','Petersen',33,'2022-11-20 07:05:22','Rio Negro');
-/*!40000 ALTER TABLE `people` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2022-11-20  4:12:34
+/*Ingreso 5 registros*/
+INSERT INTO people (nombre,apellido,edad,fecha,provincia)
+VALUES
+  ("India","Rosales",73,CURRENT_TIMESTAMP,"La Pampa"),
+  ("Tad","English",52,CURRENT_TIMESTAMP,"Buenos Aires"),
+  ("Quamar","Francis",20,CURRENT_TIMESTAMP,"Tucumán"),
+  ("Ronan","Holloway",46,CURRENT_TIMESTAMP,"Jujuy"),
+  ("Kathleen","Petersen",33,CURRENT_TIMESTAMP,"Rio Negro");
+ 
